afd23417c17b5a5b90d6947ebbaac3379c12bc45
faltuond <0xdrafon@gmail.com>
2020-04-24 12:57:14 +0200

    Implementation of First PK Stress function for Blatz-Ko material

--- src/sm/Materials/HyperelasticMaterials/blatzko.C
+++ src/sm/Materials/HyperelasticMaterials/blatzko.C
@@ -19,6 +19,27 @@ namespace oofem {
 
     void BlatzKoMaterial::giveFirstPKStressVector_3d(FloatArray &answer, GaussPoint *gp, const FloatArray &vF, TimeStep *tStep) {
 
+        //invariants of deformation (invariants of C)
+        double i2 = compute_I2_C_from_F(vF);
+        double i3 = compute_I3_C_from_F(vF);
+
+        //their first derivatives
+        FloatArray di2dF, di3dF;
+        compute_dI2_C_dF(di2dF, vF);
+        compute_dI3_C_dF(di3dF, vF);
+
+        //assembling the stress vector
+        // P = mu/(2I_3) * (dI2dF + (sqrt(I_3) - (I_2/I_3)) * dI3dF)
+        
+        answer = di3dF;
+
+        double dI3coeff = - sqrt(i3) + (i2 / i3);
+        answer.times(dI3coeff);
+
+        answer.add(di2dF);
+        
+        double mainCoeff = mu / (2 * i3);
+        answer.times(mainCoeff);
     }
 
     MaterialStatus* BlatzKoMaterial::CreateStatus(GaussPoint* gp) const {
